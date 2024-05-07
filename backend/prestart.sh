05b6de80ba55f2de2c336898c20a54fe31293a78
Destiny Saturday <yiradesat@gmail.com>
2024-05-07 09:13:37 +0100

    chore: repo setup

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
 # Let the DB start
-python3 /gitearn/app/backend_pre_start.py
+python3     ./app/backend_pre_start.py
 
 # Run migrations
 alembic upgrade head
 
 # Create initial data in DB
-python3 /gitern/app/initial_data.py
+python3 ./app/initial_data.py
